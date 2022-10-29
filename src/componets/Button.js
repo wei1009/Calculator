b7c7adbc8f6aa5da1e14b6cec88eaa3a79742852
weifang <xoweiox@gmail.com>
2022-10-28 20:33:30 -0400

    Calculate function added

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -1,29 +1,100 @@
 import { useContext } from "react";
 import { CalcContext } from "../context/CalcContext";
 
-function getStyleName(btn){
+function getStyleName(btn) {
     const className = {
-        "=":"equals"
+        "=": "equals"
     }
     return className[btn]
 }
 
-function Button( {value} ){
+function Button({ value }) {
 
     const { calc, setCalc } = useContext(CalcContext)
 
+    // User click comma
     const commaClick = () => {
         setCalc({
-            ...CalcContext,
+            ...calc,
             num: !calc.num.toString().includes(".") ? calc.num + value : calc.num
         })
     }
 
-    const handleBtnClick = () =>{
-        const results = {
-            ".":commaClick
+    // User click C
+    const resetClick = () => {
+        setCalc({
+            sign: "",
+            num: 0,
+            res: 0
+        })
+    }
+
+    // User click number
+
+    const handleClickButton = () => {
+        const numberString = value.toString()
+
+        let numberValue;
+        if (numberString === "0" && calc.num === 0) {
+            numberValue = "0"
+        } else {
+            numberValue = Number(calc.num + numberString)
         }
-        return results[value]()
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+
+    // User click operation
+
+    const signClick = () => {
+        setCalc({
+            sign: value,
+            res: !calc.res && calc.num ? calc.num : calc.res,
+            num: 0
+        })
+    }
+
+    // User click equals
+
+    const equalsClick = () => {
+        if (calc.res && calc.num) {
+            const math = (a, b, sign) => {
+                const result = {
+                    "+": (a, b) => a + b,
+                    "-": (a, b) => a - b,
+                    "x": (a, b) => a * b,
+                    "/": (a, b) => a / b,
+                }
+                result[sign](a, b);
+            }
+            setCalc({
+                sign: "",
+                res: math(calc.res, calc.num, calc.sign),
+                num: 0
+            })
+        }
+
+    }
+
+    const handleBtnClick = () => {
+        const results = {
+            ".": commaClick,
+            "C": resetClick,
+            "/": signClick,
+            "x": signClick,
+            "-": signClick,
+            "+": signClick,
+            "=": equalsClick
+        }
+        if (results[value]) {
+            return results[value]()
+        } else {
+            return handleClickButton()
+        }
+
     }
 
     return (
