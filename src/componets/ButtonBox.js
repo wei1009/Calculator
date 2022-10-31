bb6278ecb646e52167c6466f0bfed5c207943cd3
weifang <xoweiox@gmail.com>
2022-10-30 20:19:11 -0400

    Bug fixed

--- src/componets/ButtonBox.js
+++ src/componets/ButtonBox.js
@@ -1,6 +1,6 @@
-function ButtonBox({children}){
+function ButtonBox({ children }) {
     return (
-        <div className = "buttonBox">{children}</div>
+        <div className="buttonBox">{children}</div>
     )
 }
 
